bb529765ac4dae86294dac4d58b7bcdbea46f5ec
jaswanth <jaswanthsuresh1803@gmail.com>
2025-05-14 12:05:27 +0530

    add endpoint

--- src/utils/axiosConfig.js
+++ src/utils/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL: 'https://blog-app-backend-0ye4.onrender.com' || 'http://localhost:8080',
   headers: {
     'Content-Type': 'application/json',
   },
